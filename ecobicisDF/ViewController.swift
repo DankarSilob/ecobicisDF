6becf9563c2c169a2104969153f6eafa9cca09f5
Daniel Sierra <dsierra.lob@gmail.com>
2019-04-04 23:29:29 -0700

    test change
    test chage

--- ecobicisDF/ViewController.swift
+++ ecobicisDF/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
     @IBOutlet var mapView: MKMapView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        \
         // set initial location in Honolulu
         let initialLocation = CLLocation(latitude: 19.4284700, longitude:  -99.1276600)
         centerMapOnLocation(location: initialLocation)
